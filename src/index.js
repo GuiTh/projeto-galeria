375b3763d1e9436fb81af30940c428bcf3a19a97
GuiTh <guilhermetcarlos@hotmail.com>
2022-09-15 13:35:49 -0300

    teste

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//ARQUIVOS  SASS PARA O WEBPACK MONITORAR ELES
+import './sass/index.scss'
+
+//DEPENDENCIAS
+import "jquery"
+import "bootstrap"
+
+//ARQUIVOS JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
